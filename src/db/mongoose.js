4fead583c1b1b64781d188e0f5f714bb28985c42
zeref10 <yashchaturvedi296@gmail.com>
2021-04-20 23:10:09 +0530

    Added routes to create user and task

---
+++ src/db/mongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const url = "mongodb+srv://zeref:12349876@tasks.qawjq.mongodb.net/database?retryWrites=true&w=majority";
+
+mongoose.connect(url, {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true})
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => res.send(task)).catch((err) => res.status(400).send(err))
+})
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    user.save().then((result) => res.send(result)).catch((err) => res.status(400).send(err))
+})
+
+app.listen(port, () => console.log('Server is running on port ' + port))
