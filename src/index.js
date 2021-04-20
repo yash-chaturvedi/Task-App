a35a44d651c8e5747c528c84ae76f0549ff45ba2
zeref10 <yashchaturvedi296@gmail.com>
2021-04-21 01:54:03 +0530

    Added async await syntax

--- src/index.js
+++ src/index.js
@@ -9,42 +9,68 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => res.send(tasks)).catch((err) => res.status(500).send())
+app.get('/tasks', async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
-    const id = req.params.id
-    Task.findById(id).then((task) => {
+app.get('/tasks/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const task = await Task.findById(id)
         if(!task){
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((err) => res.status(500).send())
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-    task.save().then(() => res.status(201).send(task)).catch((err) => res.status(400).send(err))
+app.post('/tasks', async (req, res) => {
+    try{
+        const task = new Task(req.body)
+        await task.save()
+        res.status(201).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => res.send(users)).catch((err) => res.status(500).send())
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id
-    User.findById(id).then((user) => {
+app.get('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const user = await User.findById(id)
         if(!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((err) => res.status(500).send())
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-    user.save().then((result) => res.status(201).send(result)).catch((err) => res.status(400).send(err))
+app.post('/users', async (req, res) => {
+    try{
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, () => console.log('Server is running on port ' + port))
