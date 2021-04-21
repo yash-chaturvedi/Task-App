8e5831ac62fa39090585181db519ff0fc7d827cc
zeref10 <yashchaturvedi296@gmail.com>
2021-04-21 20:14:41 +0530

    Added routes to update user and task

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+//Task Routes
+
 app.get('/tasks', async (req, res) => {
     try{
         const tasks = await Task.find({})
@@ -41,6 +43,28 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const allowedUpdates = ['description', 'completed']
+    const updates = Object.keys(req.body)
+    const isValid = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValid){
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+//User Routes
+
 app.get('/users', async (req, res) => {
     try{
         const users = await User.find({})
@@ -68,6 +92,26 @@ app.post('/users', async (req, res) => {
         const user = new User(req.body)
         await user.save()
         res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const allowedUpdates = ['name', 'age', 'password', 'email']
+    const updates = Object.keys(req.body)
+    const isValid = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValid){
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
     }catch(e){
         res.status(400).send(e)
     }
