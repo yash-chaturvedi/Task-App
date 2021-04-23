73699ce54f9f3a4c94ac8f406cf041f39917bb1b
zeref10 <yashchaturvedi296@gmail.com>
2021-04-23 23:22:26 +0530

    Added hashing to save passwords

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name : {
         type: String,
         required: true,
@@ -34,5 +35,15 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    if(user.isModified()){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- src/router/user.js
+++ src/router/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { findById } = require('../models/user')
 const User = require('../models/user')
 const router = express.Router()
 
@@ -46,7 +47,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
         if(!user){
             return res.status(404).send()
         }
